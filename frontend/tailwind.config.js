7ae4fdd86efe8fa85ad20c6e8230bf70aee9e195
nikhil chouksey <nikhilranger18@gmail.com>
2025-09-07 14:50:33 +0530

    postcss config file added to resolve tailwind deployment issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",           // if your index.html is in project root
+    "./src/**/*.{js,ts,jsx,tsx}" // all React components
   ],
   theme: {
     extend: {},
