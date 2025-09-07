c7b65599318f41cff084663ae44ff0246a8715cc
nikhil chouksey <nikhilranger18@gmail.com>
2025-09-07 15:11:53 +0530

    cors problem resolved

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ connectToMongoDB(process.env.MONGODB_URL)
 
 app.use(express.json()); 
 
-app.use(cors({origin:process.env.FRONTEND_URL || '*'}));
+app.use(cors({ origin: [process.env.FRONTEND_URL, "http://localhost:5173"] }));
 
 app.use(express.urlencoded({ extended: true }));
 
